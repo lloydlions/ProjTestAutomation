2e1ea5a6625dfafd47ec115f082397e578b2b311
Lloyd Liongson <wb-lloydlion383444@antfin.com>
2021-02-09 23:02:41 +0800

    first project

---
+++ src/test/java/SampleClass.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class SampleClass {
+
+    @Test
+    public void testRun(){
+        String path = System.getProperty("user.dir");
+        System.setProperty("webdriver.chrome.driver", path + "/lib/webdriver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://formy-project.herokuapp.com/form");
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("first-name")));
+        driver.findElement(By.id("first-name")).sendKeys("First Name");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("last-name")));
+        driver.findElement(By.id("last-name")).sendKeys("Last Name");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Submit')]")));
+        driver.findElement(By.xpath("//a[contains(text(),'Submit')]")).click();
+        driver.close();
+    }
+}
