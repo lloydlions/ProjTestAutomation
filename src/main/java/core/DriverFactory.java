0dc49528f4272f7c7f901dc1b64b78672b92f24a
lloyd.liongson <lloyd.liongson@mynt.xyz>
2021-03-01 23:54:40 +0800

    added functionality for headless on chrome and firefox, added functional for getproperty

--- src/main/java/core/DriverFactory.java
+++ src/main/java/core/DriverFactory.java
@@ -5,76 +5,112 @@ import io.github.bonigarcia.wdm.config.DriverManagerType;
 import io.github.bonigarcia.wdm.managers.ChromiumDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.testng.annotations.BeforeSuite;
+import sun.java2d.loops.ProcessPath;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Properties;
 
 public class DriverFactory {
     private String browser;
+    private String headless;
+    private String windowLength;
+    private String windowHeight;
     Class<? extends WebDriver> driverClass = null;
+    FileInputStream fi;
+    Properties prop;
 
-    public String getBrowser() {
+    public String getPropValue(String key) throws Exception {
+        fi = new FileInputStream("./src/test/resources/config.properties");
+        prop = new Properties();
+        prop.load(fi);
+
+        return prop.getProperty(key);
+    }
+
+    public String getBrowserType(){
+        try{
+            if(browser == null){
+                browser = "chrome";
+            }else{
+                browser = getPropValue("browser");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return browser;
     }
 
-    public void setBrowser(String browser) {
-        this.browser = browser;
-    }
-
-    public WebDriver initialize() {
+    public WebDriver initialize(){
         WebDriver driver = null;
-        if(browser == null){
-            setBrowser("CHROME");
-        }
 
-        switch (getBrowser().toLowerCase()){
-            case "chrome":
-//                driverClass = ChromeDriver.class;
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
-                break;
-            case "firefox":
-//                driverClass = FirefoxDriver.class;
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-                break;
-            case "ie":
+        try {
+            switch (getBrowserType().toUpperCase()){
+                case "CHROME":
+                    WebDriverManager.chromedriver().setup();
+                    if(getPropValue("headless").equals("yes")){
+                        ChromeOptions chromeOptions = new ChromeOptions();
+                        chromeOptions.addArguments("--headless");
+                        chromeOptions.addArguments("--disable-gpu");
+                        driver = new ChromeDriver(chromeOptions);
+                    }else{
+                        driver = new ChromeDriver();
+                    }
+                    break;
+                case "FIREFOX":
+                    WebDriverManager.firefoxdriver().setup();
+                    if(getPropValue("headless").equals("yes")){
+                        FirefoxOptions ffOptions = new FirefoxOptions();
+                        ffOptions.setHeadless(true);
+                        driver = new FirefoxDriver(ffOptions);
+                    }else{
+                        driver = new FirefoxDriver();
+                    }
+                    break;
+                case "IE":
 //                driverClass = InternetExplorerDriver.class;
-                WebDriverManager.iedriver().setup();
-                driver = new InternetExplorerDriver();
-                break;
-            case "edge" :
+                    WebDriverManager.iedriver().setup();
+                    driver = new InternetExplorerDriver();
+                    break;
+                case "EDGE" :
 //                driverClass = EdgeDriver.class;
-                WebDriverManager.edgedriver().setup();
-                driver = new EdgeDriver();
-                break;
-            case "opera":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                case "OPERA":
 //                driverClass = OperaDriver.class;
-                WebDriverManager.operadriver().setup();
-                driver = new OperaDriver();
-                break;
-            case "safari":
-                try {
-                    DriverManagerType safari = DriverManagerType.SAFARI;
-                    WebDriverManager.getInstance(safari).setup();
-                    Class<?> safariClass = Class.forName(safari.browserClass());
-                    driver = (WebDriver) safariClass.getDeclaredConstructor().newInstance();
-                } catch (InstantiationException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
-                } catch (NoSuchMethodException e) {
-                    e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-                break;
+                    WebDriverManager.operadriver().setup();
+                    driver = new OperaDriver();
+                    break;
+                case "SAFARI":
+                    try {
+                        DriverManagerType safari = DriverManagerType.SAFARI;
+                        WebDriverManager.getInstance(safari).setup();
+                        Class<?> safariClass = Class.forName(safari.browserClass());
+                        driver = (WebDriver) safariClass.getDeclaredConstructor().newInstance();
+                    } catch (InstantiationException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchMethodException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
 
         return driver;
